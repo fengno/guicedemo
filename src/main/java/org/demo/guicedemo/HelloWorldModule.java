e6aad417f08872cd0b5eaf8b0a99b2e3f501d973
fzj <fengzongjian@yeah.net>
2018-04-28 14:05:15 +0800

    让HelloWorld通过命令行决定启动哪个Applet
    增加友好提示：wrong applet name, valid names are: hello, println

--- src/main/java/org/demo/guicedemo/HelloWorldModule.java
+++ src/main/java/org/demo/guicedemo/HelloWorldModule.java
@@ -10,14 +10,15 @@ import org.demo.guicedemo.hellodemo.StringWritingApplet;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import com.google.inject.multibindings.MapBinder;
 import com.google.inject.name.Names;
 
 public class HelloWorldModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
-/*		MapBinder.newMapBinder(binder(), String.class, MyApplet.class).addBinding("hello")
-				.to(StringWritingApplet.class);*/
+		MapBinder.newMapBinder(binder(), String.class, MyApplet.class).addBinding("hello")
+				.to(StringWritingApplet.class);
 		bind(MyApplet.class).annotatedWith(Names.named("hello")).to(StringWritingApplet.class);
 		bind(MyDestination.class).to(PrintStreamWriter.class);
 		bind(PrintStream.class).toInstance(System.out);
--- src/main/java/org/demo/guicedemo/PrintLineModule.java
+++ src/main/java/org/demo/guicedemo/PrintLineModule.java
@@ -3,16 +3,16 @@ package org.demo.guicedemo;
 import org.demo.guicedemo.hellodemo.PrintLineApplet;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.name.Names;
+import com.google.inject.multibindings.MapBinder;
 
 public class PrintLineModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
-		bind(MyApplet.class).annotatedWith(Names.named("println")).to(PrintLineApplet.class);
+//		bind(MyApplet.class).annotatedWith(Names.named("println")).to(PrintLineApplet.class);
 
-/*		MapBinder.newMapBinder(binder(), String.class, MyApplet.class).addBinding("println")
-				.to(PrintLineApplet.class);*/
+		MapBinder.newMapBinder(binder(), String.class, MyApplet.class).addBinding("println")
+				.to(PrintLineApplet.class);
 	}
 
 }
