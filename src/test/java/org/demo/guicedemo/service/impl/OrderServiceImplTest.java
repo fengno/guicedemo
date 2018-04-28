ccf2411d4caa5cc7f0025300492cdb813ae040b7
fzj <fengzongjian@yeah.net>
2018-04-28 10:02:10 +0800

    Module的相互关系
    1.并列；2.嵌套；3.覆盖

--- src/test/java/org/demo/guicedemo/service/impl/OrderServiceImplTest.java
+++ src/test/java/org/demo/guicedemo/service/impl/OrderServiceImplTest.java
@@ -1,6 +1,7 @@
 package org.demo.guicedemo.service.impl;
 
 import java.util.List;
+import java.util.Set;
 
 import javax.inject.Inject;
 
@@ -9,39 +10,50 @@ import org.demo.guicedemo.service.PriceService;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Provider;
 import com.google.inject.name.Named;
+import com.google.inject.util.Modules;
+
+// mock
+class PriceServiceMock extends PriceServiceImpl {
+
+	@Inject
+	public PriceServiceMock(Set<String> supportedCurrencies) {
+		super(supportedCurrencies);
+	}
+
+	@Override
+	public long getPrice(long orderId) {
+		return 5678L;
+	}
+}
 
 public class OrderServiceImplTest {
 	@Inject
 	private OrderService orderService;
-	@Inject 
+	@Inject
 	private PriceService priceService;
-	
+
 	@Inject
 	@Named("supportedCurrencies")
 	private Provider<List<String>> supportedCurrenciesProvider;
-	
-	@Before 
+
+	@Before
 	public void setUp() {
 		// 没有这一行，orderService会是null
-		Guice.createInjector(new ServerModule()/*,
-				new AbstractModule() {
-					
-					@Override
-					protected void configure() {
-						// 连接绑定
-						bind(PriceServiceImpl.class).toInstance(new PriceServiceImpl() {
-							@Override
-							public long getPrice(long orderId) {
-								return 567L;
-							}
-						});
-					}
-				}*/).injectMembers(this);
+		Guice.createInjector(
+			Modules.override(new ServerModule()) // 之前ServerModule中已经给PriceService绑定到了PriceServiceImpl，这里需Module覆盖
+			.with(new AbstractModule() {
+				@Override
+				protected void configure() {
+					bind(PriceService.class).to(PriceServiceMock.class);
+				}
+			})
+		).injectMembers(this);
 	}
-	
+
 	@Test
 	public void testSendToPayment() {
 		orderService.sendToPayment(789L);
