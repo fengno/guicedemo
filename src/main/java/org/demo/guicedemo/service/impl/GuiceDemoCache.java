9f8b0c8a0640d47a9f1aeee89c7dcd9055035d51
fzj <fengzongjian@yeah.net>
2018-05-09 20:41:13 +0800

    scope演示（singleton)

--- src/main/java/org/demo/guicedemo/service/impl/GuiceDemoCache.java
+++ src/main/java/org/demo/guicedemo/service/impl/GuiceDemoCache.java
@@ -1,12 +1,16 @@
 package org.demo.guicedemo.service.impl;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import com.google.common.cache.AbstractCache;
+import com.google.inject.Singleton;
 
+@Singleton
 public class GuiceDemoCache extends AbstractCache<String, String> {
-	private final Map<String, String> keyValues = new HashMap<>();
+//	private final Map<String, String> keyValues = new HashMap<>();
+//	如果是Singleton范围，考虑线程安全，建议用ConcurrentHashMap
+	private final Map<String, String> keyValues = new ConcurrentHashMap<>();
 
 	@Override
 	public String getIfPresent(Object key) {
--- src/test/java/org/demo/guicedemo/service/impl/CacheTest.java
+++ src/test/java/org/demo/guicedemo/service/impl/CacheTest.java
@@ -1,23 +1,13 @@
 package org.demo.guicedemo.service.impl;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-
-import java.util.List;
-import java.util.Set;
 
 import javax.inject.Inject;
 
-import org.demo.guicedemo.service.OrderService;
-import org.demo.guicedemo.service.PriceService;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
-import com.google.inject.Provider;
-import com.google.inject.name.Named;
-import com.google.inject.util.Modules;
 
 public class CacheTest {
 	@Inject
@@ -34,7 +24,7 @@ public class CacheTest {
 	public void testCache() {
 		paymentService.putCache("testKey", "testValue");
 		String cachedValue = priceService.getCachedValue("testKey");
-		// 默认scope下，paymentService下设置的cache内容，在priceService中是取不到的
-		assertNull(cachedValue);
+		// singleton scope下，paymentService下设置的cache内容，在priceService中是可以取到的
+		assertEquals("testValue", cachedValue);
 	}
 }
